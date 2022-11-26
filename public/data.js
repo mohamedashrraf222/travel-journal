1a872112891a82af0a7bab24f48083621d823f8f
Mohamed Ashraf <ma1913920@gmail.com>
2022-11-26 23:31:06 +0200

    last modefied

--- public/data.js
+++ public/data.js
@@ -6,27 +6,24 @@ export default [
         startDate: "12 Jan, 2021",
         endDate: "24 Jan, 2021",
         description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        // imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
         imageUrl: "photo-1589308078059-be1415eab4c3.jfif"
     },
     {
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "Sydney Opera House",
+        location: "AUSTRALIA",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        // imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-        imageUrl: "photo-1589308078059-be1415eab4c3.jfif"
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: "https://s3-alpha-sig.figma.com/img/453c/c12b/def81107aa5aad5460f55188541d919b?Expires=1670198400&Signature=TmO1g9HT2AvRRfu5RUaUJQbceEMochma2DH-VoOqPTupdCceA~mlLULzglXBGKLArRtzmFrh1wYMz4in7VQmoO4rYJmmTHR4bZhutJosIkVQ70hL5lB9ooZx3o~TSh337eN-02Zotvnib3zbIZFSYKZrHZYFDErPmKie1gUZVcuuvDtWW3nT9i4SRe89QigiC6-4D2DKQWrVXbPlSAUv33NISsPCizR4S9KVRCDZofkzNnuYASRaJooa9wri0exA6V0158E2oItDkKdvx4AFk1H5b8mWFDDrYMZ5U~3V-Y3v23-x2l2yeRuEXr~BC2NfDxhqzXMkXe8NHj1JJgWPew__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
     },
     {
-        title: "Mount Fuji",
-        location: "Japan",
+        title: "Geirangerfjord",
+        location: "NORWAY",
         googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-        startDate: "12 Jan, 2021",
-        endDate: "24 Jan, 2021",
-        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-        // imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
-        imageUrl: "photo-1589308078059-be1415eab4c3.jfif"
+        startDate: "01 Oct, 2021",
+        endDate: "18 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://s3-alpha-sig.figma.com/img/3f9e/2853/2b603d06d4e8dad764816cf47afc31dd?Expires=1670198400&Signature=GbSmLVDxExktoZx8Vq3fs-yHzYkf2tnCIszU3dktUzaU7waseIqh7YS2s3E-Jl4KavpxPolf7VcSYo1lhS0yed9cDO9OTIhv7MBT-YzW8XhYFG81~4fXvtRGryN9mz4~p2Jl8NZXA3ip0CZ3s51MN094kol6JzBYVKBYTv0Fdx9792QOAnTh~tKIykLf7oKeUIt-ohqlSNf~Ws0y~RooyFniIJtV53nNpmQTKlUfzgyl1P6im1T06Vn0RWBLUow3hWejl5m8gGusPXeq3pCFsCgemsxh0RFWqFeLlm2vS6wW5rvu6LGmKpy3KQWHngvIdE0fVQRnpFuH0DZW-3fWLA__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA"
     }
 ]
